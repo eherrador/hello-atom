6d01f37364d80be8b77345474c3df349f04e65c0
Edgar Herrador <eherrador@gmail.com>
2014-09-18 20:02:29 -0500

    image for using like an icon

--- hello-app/main.js
+++ hello-app/main.js
@@ -23,7 +23,7 @@ app.on('window-all-closed', function() {
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/static/andika-logo-250.png'});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
